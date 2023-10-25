44eb6d32139771c50a1ac17049a177003df28ac4
John James <john.jameschar@gmail.com>
2023-10-24 20:45:39 -0400

    adds input event to ant. Responds to click

--- src/scenes/scripts/anto.gd
+++ src/scenes/scripts/anto.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+var cursor_in = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,17 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_mouse_entered():
+	cursor_in = true
+
+
+func _on_mouse_exited():
+	cursor_in = false
+
+
+func _on_input_event(viewport, event, shape_idx):
+	print(event is InputEventMouseButton)
+	if (event is InputEventMouseButton && event.pressed):
+		print("Clicked")
